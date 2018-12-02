ac133e0abdc35dc0c34f34d55f2e96650e398a43
Batoch <baptiste.massardier@gmail.com>
2018-12-02 23:48:22 +0100

    rfdygh

--- lecture.c
+++ lecture.c
@@ -27,7 +27,8 @@ Metriques enregilecturestrer(){          //lit le fichier de donnees "fichier.tx
         i++;
     }
 
-    fseek(fic, 1, SEEK_CUR);
+    //fseek(fic, 1, SEEK_CUR);
+    fseek(fp, 0, SEEK_SET);
 
     int tabdateArrive[i];
     int tabdureAttente[i];
