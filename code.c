3f02d39f23cfe1f5311cbee0b02bf6a58563d520
Batoch <baptiste.massardier@gmail.com>
2018-12-03 00:50:37 +0100

    sf

--- code.c
+++ code.c
@@ -202,7 +202,7 @@ void tableaudebord(Metriques metriques){              //metriques est un tableau
 	fprintf(fic, "Metriques journalieres:\n\n");
     fprintf(fic, "Taille moyenne de la file d'attente: %f personnes\n", taillemoyfileattente);
     fprintf(fic, "Taille maximum de la file d'attente: %d personnes\n", taillemaxfile);
-    fprintf(fic, "Debit moyen: %f personnes\n", debitmoyen);
+    fprintf(fic, "Debit moyen: %f personnes par minutes\n", debitmoyen);
     fprintf(fic, "Taux de clients non servis: %d personnes\n", tauxclientnonservis);
     fprintf(fic, "Temps de traitement moyen: %f minutes\n", tempsreponsemoy);
 
