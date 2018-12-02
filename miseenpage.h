f74fee7fbfefeddb7cabf618ccb7fa42aa0ffbfc
Batoch <baptiste.massardier@gmail.com>
2018-12-03 00:45:00 +0100

    pmm

--- miseenpage.h
+++
@@ -1,8 +0,0 @@
-
-typedef struct {float taillemoyfileattente;
-int taillemaxfile;
-float debitmoyen;
-float tauxclientnonservis;
-float tempsreponsemoy;    } Metriques;
-
-void tableaudebord(Metriques metriques);
